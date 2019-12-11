c5b6871c641d2895525f03cac3c955a0f08250e7
Forz70043 <alfonso.pisicchio@libero.it>
2019-12-11 20:20:26 +0100

    Added server.js with app listening on 3000

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+
+
+
+
+//ES6
+app.listen(3000,()=>{
+	console.log('listening 3000');
+});
+
+/*
+app.listen(3000, function(){
+	console.log('listening 3000');
+});
+*/
+
+
