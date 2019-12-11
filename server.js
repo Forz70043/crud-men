dc6bb024a0280f377cf8a995bfc2745874003d11
Forz70043 <alfonso.pisicchio@libero.it>
2019-12-11 20:25:02 +0100

    Add app.get for /

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require('express');
 
 const app = express();
 
-
-
+/*
+ * Req for request & res for response
+ */
+app.get('/',(req,res)=>{
+	res.send('Himotherfuckerrrr');
+	console.log(__dirname);
+});
 
 
 
@@ -16,7 +21,7 @@ app.listen(3000,()=>{
 	console.log('listening 3000');
 });
 
-/*
+/* ES5
 app.listen(3000, function(){
 	console.log('listening 3000');
 });
