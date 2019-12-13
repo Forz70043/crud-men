99c84fa36fc952d949a7c697f1095bc03854aeaa
Forz70043 <alfonso.pisicchio@libero.it>
2019-12-13 22:59:02 +0100

    Added: collection find - view engine

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 var db;
 
+app.set('view engine', 'ejs');
+
 //req from body
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 MongoClient.connect('mongodb://127.0.0.1:27017',(err,client)=>{
 	//start mongo server
 	if(err) return console.log(err);
-	db = client.db('crud-men');     //<= XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	db = client.db('crud-men');     //<= db 
 
 	app.listen(3000, ()=>{
 		console.log('listening on 3k');
@@ -24,6 +26,10 @@ MongoClient.connect('mongodb://127.0.0.1:27017',(err,client)=>{
  */
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/index.html');
+	var cursor = db.collection('quotes').find();
+	db.collection('quotes').find().toArray(function(err,results){
+		console.log(results);
+	});
 });
 
 //for req
@@ -48,3 +54,6 @@ app.listen(3000, function(){
 	console.log('listening 3000');
 });
 */
+
+
+
