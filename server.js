48933e07bf060e61e76b8b3c2e7b2a01c35520ce
Forz70043 <alfonso.pisicchio@libero.it>
2019-12-13 22:12:30 +0100

    Added: body-parser - mongodb connection & save

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+var db;
+
+//req from body
+app.use(bodyParser.urlencoded({extended: true}));
+
+//mongoConnection
+MongoClient.connect('mongodb://127.0.0.1:27017',(err,client)=>{
+	//start mongo server
+	if(err) return console.log(err);
+	db = client.db('crud-men');     //<= XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+	app.listen(3000, ()=>{
+		console.log('listening on 3k');
+	});
+});
 
 /*
  * Req for request & res for response
@@ -10,16 +26,25 @@ app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/index.html');
 });
 
+//for req
+app.post('/quotes', (req,res)=>{
+	//console.log(req.body);
+	//collection().save()deprecated			<= !!!!!!!!!!!!!!!!!
+	db.collection('quotes').save(req.body, (err,result)=>{
+		if(err) return console.log(err);
 
+		console.log('saved into db');
+		res.redirect('/');
+	});
+});
 
 //ES6
-app.listen(3000,()=>{
-	console.log('listening 3000');
-});
+//app.listen(3000,()=>{
+//	console.log('listening 3000');
+//});
 
 /* ES5
 app.listen(3000, function(){
 	console.log('listening 3000');
 });
 */
-
