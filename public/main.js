0db1c2d8c9dd15734d5ff53847fb75ec018e3238
Forz70043 <alfonso.pisicchio@libero.it>
2019-12-14 16:16:27 +0100

    Added put function and view

--- public/main.js
+++ public/main.js
@@ -1,6 +1,23 @@
+//window.onload = function(){
 var update= document.getElementById('update');
+console.log(update);
 
-update.addEventListener('click',()=>{
+update.addEventListener('click',function(){
 	console.log('event listener');
-
+	fetch('quotes', {
+	  method: 'put',
+	  headers: {'Content-Type': 'application/json'},
+	  body: JSON.stringify({
+	    'name': 'XXX Hi MotherFucker',
+	    'quote': 'I\'ll kill you!'
+	  })
+	}).then(response=>{
+	  if(response.ok) return response.json();
+	}).then(data=>{
+	  console.log(data);
+	});
 });
+
+//}
+
+
--- server.js
+++ server.js
@@ -9,8 +9,9 @@ var db;
 app.set('view engine', 'ejs');
 //req from body
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 //view part
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 //mongoConnection
 MongoClient.connect('mongodb://127.0.0.1:27017',(err,client)=>{
@@ -29,7 +30,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017',(err,client)=>{
  */
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/index.html');
-	var cursor = db.collection('quotes').find();
+	var cursor = db.collection('quotes').find();   //<=NON UTILIZZO?
 	db.collection('quotes').find().toArray(function(err,results){
 		//console.log(results);
 		if(err) return console.log(err);
@@ -39,13 +40,29 @@ app.get('/',(req,res)=>{
 
 //req save
 app.post('/quotes', (req,res)=>{
-	//console.log(req.body);
 	//collection().save()deprecated			<= !!!!!!!!!!!!!!!!!
 	db.collection('quotes').save(req.body, (err,result)=>{
 		if(err) return console.log(err);
 		//console.log('saved into db');
 		res.redirect('/');
 	});
+});
+
+app.put('/quotes',(req,res)=>{
+	db.collection('quotes').findOneAndUpdate(
+		{ name: 'XXX Hi Guys'},
+		{ $set:
+			{ 
+			  name: req.body.name,
+			  quote: req.body.quote
+			}
+		},
+		{ sort: {_id: -1},
+		  upsert: true
+		},(err,result)=>{
+		if(err) return res.send(err);
+		res.send(result);
+		});
 });
 
 
