2c1c5db0ef029250ab0cd956cb035394780c3e91
Forz70043 <alfonso.pisicchio@libero.it>
2021-04-29 14:48:40 +0200

    fix footer

--- doc/schema.sql
+++ doc/schema.sql
@@ -26,3 +26,33 @@ CREATE TABLE `SYSLOG` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `ROLE`(
+    `id` int(21) unsigned AUTO_INCREMENT not null,
+    `name` varchar(256) not null,
+    PRIMARY KEY(`id`)
+);
+
+
+CREATE TABLE `USERS`(
+    `id` int(21) unsigned AUTO_INCREMENT not null,
+    `name` varchar(256) not null,
+    `surname` varchar(256) not null,
+    `email` varchar(256) not null,
+    `password` varchar(256) not null,
+    `photo` text default null,
+    `gender` tinyint(0),
+    `yearOfBirth` DATE not null,
+    `cod_id` varchar(256) DEFAULT NULL,
+    PRIMARY KEY(`id`),
+);
+
+CREATE TABLE `COUNTRY` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `iso_3166` char(2) DEFAULT '',
+  `blacklist` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `iso_3166` (`iso_3166`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
