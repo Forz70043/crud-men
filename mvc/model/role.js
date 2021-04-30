6f8f3293f2761c4d1a961221ae19d50bbda61325
Forz70043 <alfonso.pisicchio@libero.it>
2021-04-30 10:39:15 +0200

    Added Model Role

---
+++ mvc/model/role.js
@@ -0,0 +1,32 @@
+var Entity = require('../../entity');
+
+class Role extends Entity {
+    constructor(){
+        super();
+        this.TBL='ROLE';
+        this.fields={'name':{}};
+    }
+
+    async get(){
+        var result = await this.find(false,false);
+        console.log("GET ALL :",result);
+        return result;
+    }
+
+    async add(params){
+        console.log(" ADD ",params);
+		console.log(Object.keys(params).length);
+
+		var objKeys = Object.keys(params);
+		if(objKeys.length>0){
+			console.log("params >0");
+			var result = await this.insertQuery(params);
+			console.log(" ADD: ",result);
+			return result;
+		}
+		else console.log("params <0");
+    }
+
+}
+
+module.exports = Role;
